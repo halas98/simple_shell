ce2e023c3e9e62bd55840d767d1afc96827ad714
halas98 <salahkalada4@gmail.com>
2023-08-23 16:44:31 -0700

    updt

---
+++ help2.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
+
+/**
+ * help_env - Provides details about the 'env' built-in command in shellby.
+ */
+
+void help_env(void)
+{
+	char *msg = "env: env\n\tPrints the current environment.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+/**
+ * help_setenv - Provides details about the 'setenv' built-in command in shellby.
+ */
+
+void help_setenv(void)
+{
+	char *msg = "setenv: setenv [VARIABLE] [VALUE]\n\tInitializes a new";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environment variable, or modifies an existing one.\n\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\tUpon failure, prints a message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * help_unsetenv - Provides details about the 'unsetenv' built-in command in shellby.
+ */
+
+void help_unsetenv(void)
+{
+	char *msg = "unsetenv: unsetenv [VARIABLE]\n\tRemoves an ";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "environmental variable.\n\n\tUpon failure, prints a ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "message to stderr.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
